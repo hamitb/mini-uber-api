25f6ade995353fa718e5b57a0c5b15ea71f62b69
Yasin Uysal <ayasinuysal@gmail.com>
2022-08-20 16:08:02 +0300

    Add Hello End-point

--- src/main/java/com/bootcamp/uber/HelloController.java
+++ src/main/java/com/bootcamp/uber/HelloController.java
@@ -1,6 +1,7 @@
 package com.bootcamp.uber;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World!";
+    @GetMapping("/hello/{name}/{age}")
+    public String sayHello(@PathVariable String name, @PathVariable int age) {
+        return "Hello " + name + " ! " + "Your age is " + age ;
     }
 
 }
